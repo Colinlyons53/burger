7403ddd98d754bfa5810bfc507fec834e3de495a
Colin Lyons <Colinlyons53@gmail.com>
2019-07-17 20:43:52 -0700

    set up server.js to listen at port 3000.

--- server.js
+++ server.js
@@ -4,3 +4,17 @@ var methodOverride = require('method-override');
 var exphbs = require('express-handlebars');
 
 var app = express();
+app.use(express.static(_dirname + '/public'));
+
+app.use(body.parser.urlencoded({
+    extended: false
+}))
+
+app.use(methodOverride('method'));
+app.engine('handlebars',exphbs({
+    defaultLayout: 'main'
+}));
+app.set('view engine', 'handlebars');
+
+var port = 3000;
+app.listen(port)
